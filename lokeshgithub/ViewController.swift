9624127f5e41d77f12682087f8216aaa199e901b
Lokesh chinatatareddy <lokeshchinatatareddy@achiever-9.local>
2018-07-10 09:53:29 +0530

    sec Commit

--- lokeshgithub/ViewController.swift
+++ lokeshgithub/ViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ var name = "mahesh"
     override func viewDidLoad() {
+       
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
     }
 
     override func didReceiveMemoryWarning() {
